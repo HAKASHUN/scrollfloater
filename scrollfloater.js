71283bc77900b1e22debf97e247900c531f4ba67
Shunsuke Hakamata <hakashun@hakashun.net>
2013-04-11 19:54:49 +0900

    Test Commit
    My First Github Commit.

---
+++ scrollfloater.js
@@ -0,0 +1,9 @@
+/**
+ * scrollfloater.js
+ *
+ * License: MIT (http://www.opensource.org/licenses/mit-license)
+ * GitHub: https://github.com/HAKASHUN/scrollfloater
+ * @author HAKASHUN
+ */
+
+//hoge
